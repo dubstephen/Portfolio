58fdce9f8f4cfc59b2d75fcc73ff5fa5bb1c5ee8
Stephen Bloodworth <stephenbloodworth72@gmail.com>
2024-05-13 14:12:46 -0500

    Add contact section and css

---
+++ frontend/next-portfolio/app/components/ContactMe.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './ContactMe.css'
+
+const ContactMe = () => {
+  return (
+    <>
+      <section id='contact-me' className='text-black h-screen bg-white text-center contact-me'>
+        <h1>Contact Me</h1>
+      </section>
+    </>
+  )
+}
+
+export default ContactMe
