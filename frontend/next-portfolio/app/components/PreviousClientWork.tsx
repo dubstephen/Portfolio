b9fb98d9f5b71ac0e5ef3e527cf2ade8bab82490
Stephen Bloodworth <stephenbloodworth72@gmail.com>
2024-05-13 14:13:44 -0500

    Add previous client work section componant and css

---
+++ frontend/next-portfolio/app/components/PreviousClientWork.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './PreviousClientWork.css'
+
+const PreviousClientWork = () => {
+  return (
+    <>
+      <section id='previous-client-work' className='text-white h-screen bg-blue-950 text-center previous-client-work'>
+        <h1>Previous Client Work</h1>
+      </section>
+    </>
+  )
+}
+
+export default PreviousClientWork
