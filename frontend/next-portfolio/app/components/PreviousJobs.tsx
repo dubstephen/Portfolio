bf08793518b72973394bde4480cb3d2e76b4ed4e
Stephen Bloodworth <stephenbloodworth72@gmail.com>
2024-05-13 14:14:05 -0500

    Add previous jobs section componant and css

---
+++ frontend/next-portfolio/app/components/PreviousJobs.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './PreviousJobs.css'
+
+const PreviousJobs = () => {
+  return (
+    <>
+      <section id='previous-jobs' className='text-white h-screen bg-blue-950 text-center previous-jobs'>
+        <h1>Where I've Worked</h1>
+      </section>
+    </>
+  )
+}
+
+export default PreviousJobs
