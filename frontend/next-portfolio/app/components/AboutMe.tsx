ba6b006b9ade0f4043862e88f2835e96b7108444
Stephen Bloodworth <stephenbloodworth72@gmail.com>
2024-05-13 14:12:22 -0500

    Add about me section componant and css

---
+++ frontend/next-portfolio/app/components/AboutMe.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './AboutMe.css'
+
+const AboutMeSection = () => {
+  return (
+    <>
+      <section id='about' className='text-white h-screen bg-blue-950 text-center about'>
+        <h1>About Me</h1>
+      </section>
+    </>
+  )
+}
+
+export default AboutMeSection
