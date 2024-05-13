70ffeb80aa7bbcb598c97541de0f17a5718b8054
Stephen Bloodworth <stephenbloodworth72@gmail.com>
2024-05-13 14:12:01 -0500

    Add hero section componant and css

---
+++ frontend/next-portfolio/app/components/HeroSection.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const HeroSection = () => {
+  return (
+    <>
+      <section id='hero' className='text-white h-screen bg-blue-950 text-center hero'>
+        <h1>Hero</h1>
+      </section>
+    </>
+  )
+}
+
+export default HeroSection
