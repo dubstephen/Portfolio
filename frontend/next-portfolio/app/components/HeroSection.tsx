56c493a2524cc2436c076c499c975584b883d3ae
Stephen Bloodworth <stephenbloodworth72@gmail.com>
2024-05-19 11:16:16 -0500

    Hero section adjustments

--- frontend/next-portfolio/app/components/HeroSection.tsx
+++ frontend/next-portfolio/app/components/HeroSection.tsx
@@ -4,12 +4,12 @@ import './HeroSection.css';
 const HeroSection = () => {
   return (
     <>
-      <section id='hero' className='text-white h-screen bg-gradient-to-b from-black-pearl to-big-stone flex text-center md:text-left hero'>
-        <div className='hero-container'>
+      <section id='hero' className=' text-white bg-gradient-to-b from-black-pearl to-big-stone flex text-center md:text-left'>
+        <div className='ml-auto mr-auto md:mb-64 mt-32 md:mt-64 hero-container'>
           <p className='p-2 introduction text-bismark text-base md:text-base '>Hi, my name is</p>
-          <h1 className='p-2 name text-cadet-blue text-4xl md:text-5xl '>Stephen Bloodworth.</h1>
-          <h2 className='p-2 what-i-do text-flord text-4xl md:text-5xl '>I build software.</h2>
-          <p className='pt-8 md:p-2 hero-description text-flord text-lg md:text-xl '>
+          <h1 className='p-2 md:mt-4 name text-cadet-blue text-4xl md:text-5xl '>Stephen Bloodworth.</h1>
+          <h2 className='p-2 md:mt-4 what-i-do text-flord text-4xl md:text-5xl '>I build software.</h2>
+          <p className='pt-8 md:mt-4 md:p-2 hero-description text-flord text-lg md:text-xl '>
             I am a software engineer specializing in building highly performant<br/>
             applications that solve real-world problems and provide users with an<br/>
             awesome experience. I’m currently working using Spring Boot and Angular
