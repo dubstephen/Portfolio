3a973c096a5791e25640e24f4a30c6b3329f5bb4
Stephen Bloodworth <stephenbloodworth72@gmail.com>
2024-05-19 11:25:57 -0500

    Tailwind config changes

--- frontend/next-portfolio/tailwind.config.ts
+++ frontend/next-portfolio/tailwind.config.ts
@@ -1,19 +1,23 @@
+import {nextui} from '@nextui-org/theme';
 import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/components/(button|modal|ripple|spinner).js"
   ],
   theme: {
     extend: {
       colors: {
-        'black-pearl': '#08192E',
         'big-stone': '#122439',
-        'flord': '#607187',
-        'cadet-blue': '#B9C5D4',
         'bismark': '#5F97A7',
+        'black-pearl': '#08192E',
+        'blue-Dianne': '#244B56',
+        'blue-Zodiac': '#102F54',
+        'cadet-blue': '#B9C5D4',
+        'flord': '#607187',
       },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
@@ -22,6 +26,6 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [nextui()],
 }
 export default config
