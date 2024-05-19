e8a0ad077f3f3072269b7b9c8cbfbe266e81c246
Stephen Bloodworth <stephenbloodworth72@gmail.com>
2024-05-19 11:25:31 -0500

    Hide personal projects section

--- frontend/next-portfolio/app/page.tsx
+++ frontend/next-portfolio/app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
       <HeroSection />
       <AboutMe />
       <PreviousJobs />
-      <PersonalProjects />
+      {/* <PersonalProjects /> */}
       <PreviousClientWork />
       <ContactMe />
     </main>
