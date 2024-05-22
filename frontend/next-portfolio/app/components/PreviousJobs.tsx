6cf8baa8b5c3b7cf51f6e9d4df3b3b4d4d1cf6f1
Stephen Bloodworth <stephenbloodworth72@gmail.com>
2024-05-22 13:01:05 -0500

    PreviousJobs tweaks

--- frontend/next-portfolio/app/components/PreviousJobs.tsx
+++ frontend/next-portfolio/app/components/PreviousJobs.tsx
@@ -1,12 +1,20 @@
-import React from 'react'
+'use client';
+import React, { useRef, useState } from 'react'
 import './PreviousJobs.css'
 import LORModal from './LORModal'
+import { useIsVisible } from '../helpers';
 
 const PreviousJobs = () => {
+  const [showSection, setShowSection] = useState<boolean>(false);
+  const ref = useRef();
+  const isVisible = useIsVisible(ref);
+  if (isVisible && !showSection) {
+    setShowSection(true);
+  }
   return (
     <>
-    <section id='previous-jobs' className='text-white bg-big-stone justify-center h-max md:justify-start md:text-left flex previous-jobs'>
-        <div className="pl-8 pr-8 md:ml-auto md:mr-auto h-screen md:mb-64 mt-32 md:mt-64">
+    <section id='previous-jobs' className='text-white bg-big-stone justify-center md:justify-start md:text-left flex'>
+        <div className={`pl-8 pr-8 md:ml-auto md:mr-auto previous-jobs md:mb-0 mt-32 transition-opacity ease-in duration-700 ${showSection ? "opacity-100" : "opacity-0"}`}>
           <h3>
             <div className="flex items-center w-full">
               <span className="text-bismark text-xl md:text-2xl pr-2">02.</span>
@@ -30,12 +38,13 @@ const PreviousJobs = () => {
               <div className="h-[40px] bg-bismark w-[2px] "></div>
               <div className="flex items-center justify-start w-full">
                 <span className="text-cadet-blue text-xl md:text-2xl ml-6">Software Engineer</span>
-                <a href='https://cypressmill.co/' target="_blank" rel="noopener noreferrer" className="text-bismark text-xl md:text-2xl md:ml-2 hidden md:inline">@ Cypress Mill Co.</a>
+                <span className="text-bismark text-xl md:text-2xl md:ml-2 hidden md:inline">@</span>
+                <a href='https://cypressmill.co/' target="_blank" rel="noopener noreferrer" className="text-bismark text-xl md:text-2xl md:ml-2 hidden md:inline hover:underline"> Cypress Mill Co.</a>
               </div>
             </div>
             <div className="text-flord text-sm ml-6">April 2020 - April 2024</div>
           </h4>
-          <div className="flex justify-start items-center w-full pt-6 ml-6">
+          <div ref={ref} className="flex justify-start items-center w-full pt-6 ml-6">
             <div className="h-24">
               <div className="flex items-center justify-start w-full">
                 <p className='pt-8 md:p-2 hero-description text-bismark text-lg md:text-xl '>
@@ -52,9 +61,7 @@ const PreviousJobs = () => {
                   {'>'}<br/>
                   <br/>
                   <br/>
-                  <br/>
                   <span className='md:hidden'>
-                    <br/>
                     <br/>
                     <br/>
                   </span>
@@ -75,16 +82,15 @@ const PreviousJobs = () => {
               </div>
               <div className="flex items-center justify-start w-full">
                 <p className='pt-8 md:p-2 hero-description text-cadet-blue text-lg md:text-xl '>
-                  Work with a variety of different languages, platforms,<br/>
-                  frameworks, and content management systems such as<br/>
-                  JavaScript, TypeScript, Gatsby, React, Craft, WordPress,<br/>
-                  Prismic, and Netify
+                  Work with a variety of different languages, platforms, and <br/>
+                  frameworks such as JavaScript, TypeScript, React,<br/>
+                  Django, Python, .NET, and more!
                 </p>
               </div>
               <div className="flex items-center justify-start w-full">
                 <p className='pt-8 md:p-2 md:pb-12 hero-description text-cadet-blue text-lg md:text-xl '>
-                  Communicate with multi-disciplinary teams of engineers,<br/>
-                  designers, producers, and clients on a daily basis
+                  Communicate with multi-disciplinary teams of engineers<br/>
+                  and clients on a daily basis
                 </p>
               </div>
               <div className="flex items-center justify-start w-full">
