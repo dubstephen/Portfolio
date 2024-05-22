b1eaea4c3a9bacd31e707f562c9ee726d6eb73c9
Stephen Bloodworth <stephenbloodworth72@gmail.com>
2024-05-22 16:43:28 -0500

    Move about me description into its own component

--- frontend/next-portfolio/app/components/AboutMe.tsx
+++ frontend/next-portfolio/app/components/AboutMe.tsx
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from 'react'
 import Image from "next/image";
 import './AboutMe.css'
 import TechRow from './TechRow';
+import AboutMeDescription from './AboutMeDescription';
 
 const technologiesRow1: Technology[] = [
   {
@@ -94,69 +95,10 @@ const AboutMeSection = () => {
               <div className={`transition-opacity  ease-in duration-700 ${showSection ? "opacity-100" : "opacity-0"}`}>
                 <span className="text-cadet-blue text-md sm:text-xl md:text-3xl font-sans">About Me</span>
               </div>
-              <hr className="w-40 md:w-80 h-0.5 ml-2 my-4 bg-gray-100 border-0 rounded md:my-10 dark:bg-gray-700" />
+              <hr className="w-40 md:w-80 h-0.5 ml-2 my-4 bg-blue-Dianne border-0 rounded md:my-10 " />
             </div>
           </h3>
-          <div className={`flex justify-center transition-opacity ease-in duration-700 ${showSection ? "opacity-100" : "opacity-0"}`}>
-            <div className='flex-col font-sans'>
-              <Image className='xl:hidden mx-auto mt-8 mb-8 rounded-md' src={'/Profile Pic.jpg'} width={300} height={300} alt="logo" />
-              <div className="flex items-center justify-start w-full">
-                <p className='pt-8 md:p-2 hero-description text-cadet-blue md:text-flord text-lg md:text-lg '>
-                  Hello, My name is Stephen and I love building software!<br/>
-                  My interest in software development started in the early<br/>
-                  stages of my adult life, but I never thought I would be<br/>
-                  able to truly pursue it. it wasn't until spring of 2019 that<br/>
-                  I finally decided to dive in and see what this field had<br/>
-                  to offer.
-                </p>
-              </div>
-              <div className="flex items-center justify-start w-full">
-                <p className='pt-8 md:p-2 hero-description text-cadet-blue md:text-flord text-lg md:text-lg '>
-                  I spent the next year learning everything I could in order<br/>
-                  to launch my new career off the ground. I managed to<br/>
-                  stumble upon an amazing team of engineers at <a href='https://cypressmill.co/' target="_blank" rel="noopener noreferrer" className="text-bismark hover:underline">a software<br/> consulting firm </a>
-                  based out of Florence, AL. They took<br/>
-                  me on and trained me on everything front to<br/>
-                  back.
-                </p>
-              </div>
-              <div className="flex items-center justify-start w-full">
-                <p className='pt-8 md:p-2 hero-description text-cadet-blue md:text-flord text-lg md:text-lg '>
-                  While working there I had many opurtunities to get<br/>
-                  my hands on a wide variety of technologies<br/>
-                  <span className='hero-description text-cadet-blue text-lg md:text-lg '>Here are just a few of my favorites:</span>
-                </p>
-              </div>
-              <div className=" pt-8">
-                <div className="w-full columns-3">
-                  <div className="">
-                    { technologiesRow1.map((tech) => {
-                      return (
-                        <TechRow tech={tech} />
-                      )
-                    })}
-                  </div>
-                  <div className="">
-                    { technologiesRow2.map((tech) => {
-                      return (
-                        <TechRow tech={tech} />
-                      )
-                    })}
-                  </div>
-                  <div className="">
-                    { technologiesRow3.map((tech) => {
-                      return (
-                        <TechRow tech={tech} />
-                      )
-                    })}
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className='hidden xl:inline flex-col pt-4 pl-8'>
-              <Image src={'/Prof-Headshot.jpg'} width={300} height={300} alt="logo" />
-            </div>
-          </div>
+          <AboutMeDescription />
         </div>
       </section>
     </>
