e28560f3bcb7a18b1cea80f0b7984644c9349290
Stephen Bloodworth <stephenbloodworth72@gmail.com>
2024-05-22 13:01:15 -0500

    TechRow tweaks

--- frontend/next-portfolio/app/components/TechRow.tsx
+++ frontend/next-portfolio/app/components/TechRow.tsx
@@ -1,18 +1,24 @@
 'use client';
 import Image from "next/image";
+import { useState } from "react";
 
 interface Props {
   tech: Technology;
 }
 
 const TechRow = (props: Props) => {
+  const [hovered, setHovered] = useState<boolean>(false);
   const { tech } = props;
   return (
-    <div className='pb-16'>
-      <a href={tech.link} target="_blank" rel="noopener noreferrer">
+    <div key={'tech-item-'+tech.id} className='pb-16'>
+      <a href={tech.link} target="_blank" rel="noopener noreferrer" onMouseEnter={() => setHovered(true)} onMouseLeave={() => setHovered(false)}>
         <Image className='inline ' src={tech.imageSRC} width={40} height={40} alt="logo" />
+        { hovered ? 
+          <span className={'text-bismark underline pl-4 hidden md:inline'}>{tech.title}</span>
+        :  
+          <span className={'text-cadet-blue pl-4 hidden md:inline'}>{tech.title}</span>
+        }
       </a>
-      <span className='text-cadet-blue pl-4 hidden md:inline'>{tech.title}</span>
     </div>
   );
 }
