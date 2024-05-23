c2ffc72f8919ba73d148c1bcde97acd2aaa7f9b1
Stephen Bloodworth <stephenbloodworth72@gmail.com>
2024-05-23 14:03:46 -0500

    Trying something

--- frontend/next-portfolio/next.config.js
+++ frontend/next-portfolio/next.config.js
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: 'export',
+    distDir: 'frontend/next-portfolio/out',
     eslint: {
       ignoreDuringBuilds: true,
   },
