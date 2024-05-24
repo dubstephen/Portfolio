24d794754b68e916835cb755e88aa63f690fa3b5
Stephen Bloodworth <stephenbloodworth72@gmail.com>
2024-05-24 11:12:34 -0500

    Adjust paths

--- sb-portfolio/app/scripts/heroku-start.js
+++ sb-portfolio/app/scripts/heroku-start.js
@@ -1,19 +1,19 @@
-console.log("Did it wooooooooooooooooooooooooooooooooooooooork?")
 const express = require('express');
 const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 app.use(express.json());
+console.log("-------------------------------------------------")
 console.log("Working Directory: ", __dirname)
 // Your static pre-build assets folder
-app.use(express.static(path.join(__dirname, '..', 'build')));
+app.use(express.static(path.join(__dirname, '..')));
 // Root Redirects to the pre-build assets
 app.get('/', function(req,res){
-  res.sendFile(path.join(__dirname, '..', 'build'));
+  res.sendFile(path.join(__dirname, '..'));
 });
 // Any Page Redirects to the pre-build assets folder index.html that // will load the react app
 app.get('*', function(req,res){
-  res.sendFile(path.join(__dirname, '..', 'build/index.html'));
+  res.sendFile(path.join(__dirname, '../index.html'));
 });
 app.listen(port, ()=>{
   console.log("Server is running on port: ", port)
