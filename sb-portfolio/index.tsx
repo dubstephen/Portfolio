23cb1562f857917cdca844df0c471679b6fd8bd4
Stephen Bloodworth <stephenbloodworth72@gmail.com>
2024-05-24 11:38:30 -0500

    Restructure index tsx

--- sb-portfolio/index.tsx
+++ sb-portfolio/index.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
 import Home from './app/page';
-const container = document.getElementById('react');
-const root = createRoot(container!);
-root.render(
-  <React.StrictMode>
-    <Home />
-  </React.StrictMode>
-);
+
+export default function index() {
+  return (
+    <>
+      <Home />
+    </>
+  )
+}
