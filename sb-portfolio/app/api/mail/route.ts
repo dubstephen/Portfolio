34a974c1cf2ab7ae5aaec38f8f52fc7628729ce7
Stephen Bloodworth <stephenbloodworth72@gmail.com>
2024-05-26 12:19:57 -0500

    Address build errors

--- sb-portfolio/app/api/mail/route.ts
+++ sb-portfolio/app/api/mail/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import sgMail from "@sendgrid/mail";
+import sgMail, { MailDataRequired } from "@sendgrid/mail";
 
 export async function GET(req: Request, res: any) {
     return NextResponse.json({
@@ -8,34 +8,37 @@ export async function GET(req: Request, res: any) {
 }
 
 export async function POST(req: Request, res: any) {
-  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-  const sgEmail = process.env.SG_EMAIL;
-  const myEmail = process.env.PERSONAL_EMAIL;
-  const body = await req.json();
-  const message = `
-    Name: ${body.name}\r\n
-    Company: ${body.company}\r\n
-    Email: ${body.email}\r\n\r\n
-    ${body.message}
-  `;
-  const data = {
-    to: myEmail,
-    from: sgEmail,
-    subject: body.subject,
-    text: message,
-    html: message.replace(/\r\n/g, '<br>')
+  if (process.env.SENDGRID_API_KEY) {
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+    const sgEmail = process.env.SG_EMAIL;
+    const myEmail = process.env.PERSONAL_EMAIL;
+    const body = await req.json();
+    const message = `
+      Name: ${body.name}\r\n
+      Company: ${body.company}\r\n
+      Email: ${body.email}\r\n\r\n
+      ${body.message}
+    `;
+    const data: MailDataRequired = {
+      to: myEmail,
+      from: sgEmail,
+      subject: body.subject,
+      text: message,
+      html: message.replace(/\r\n/g, '<br>')
+    }
+  
+    try {
+      await sgMail.send(data);
+      return NextResponse.json({
+        status: 'Success',
+      })
+    } catch (e) {
+      return NextResponse.json({
+        status: 'Error',
+      })
+    }
   }
-
-  try {
-    await sgMail.send(data);
-    return NextResponse.json({
-      status: 'Success',
-    })
-  } catch (e) {
-    return NextResponse.json({
-      status: 'Error',
-    })
-  }
-
-
+  return NextResponse.json({
+    status: 'Error',
+  })
 }
