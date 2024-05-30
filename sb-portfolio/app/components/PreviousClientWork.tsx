9661e600a77a874b45d6f45c7a7e498e1d7d7543
Stephen Bloodworth <stephenbloodworth72@gmail.com>
2024-05-30 10:27:05 -0500

    Re-word client work descriptions

--- sb-portfolio/app/components/PreviousClientWork.tsx
+++ sb-portfolio/app/components/PreviousClientWork.tsx
@@ -12,6 +12,25 @@ interface Props {
 const clientCardRow1: ClientCard[] = [
   {
     id: '1',
+    title: 'International Composting Facilities',
+    description: 'Developed and implemented many new features while strategizing \
+    with a small team of engineers. Together, we built systems used across the globe to \
+    automate and maintain piles of compost.',
+    link: 'https://cypressmill.co/project-showcase/green-mountain-technologies/',
+    technologies: ['Controllers', 'React', 'Lua', 'JS', 'IoT'],
+  },
+  {
+    id: '2',
+    title: 'Rapid Emergency Notification Tool',
+    description: 'Collaborated with engineers to develop a rapid emergency notification tool to be \
+    utilized by schools. I implemented multiple features on this tool including geo-fencing and positioning.',
+    link: 'https://cypressmill.co/project-showcase/nside/',
+    technologies: ['Xamarin', 'C#'],
+  },
+]
+const clientCardRow2: ClientCard[] = [
+  {
+    id: '3',
     title: 'Case Management Platform',
     description: 'Built out new features for an online case management tool used for reducing waste \
     and helping organizations accurately allocate their funds to those in need.',
@@ -19,44 +38,24 @@ const clientCardRow1: ClientCard[] = [
     technologies: ['Django', 'Python', 'Python Templates'],
   },
   {
-    id: '2',
-    title: 'Rapid Emergency Notification Tool',
-    description: 'Collaborated with engineers to develop a rapid emergency notification tool to be \
-    utilized by schools. This tool uses geo-positioning to equip all staff to initiate an urgent \
-    response to any emergency.',
-    link: 'https://cypressmill.co/project-showcase/nside/',
-    technologies: ['Xamarin', 'C#'],
-  },
-  {
-    id: '3',
-    title: 'International Composting Facilities',
-    description: 'Strategized with a small team of engineers to build software used for monitoring \
-    and automating various processes revolving around composting while adhering to environmental regulations.',
-    link: 'https://cypressmill.co/project-showcase/green-mountain-technologies/',
-    technologies: ['Controllers', 'React', 'Lua', 'JS', 'IoT'],
-  },
-]
-const clientCardRow2: ClientCard[] = [
-  {
     id: '4',
     title: 'Luxury Coach Management Tool',
-    description: "Worked on and maintained a tool designed for managing fleets of luxury coaches to be leased out for top touring musicians.\
-    This tool also calculates pricing based on multiple factors including the selected driver's hourly rate and which cities the musician is visiting.",
+    description: "Helped build and maintain a tool designed for managing fleets of luxury coaches to be leased out for top touring musicians.\
+    I added code that would calculate pricing based on a number of factors such as hourly rates for drivers, how many stop, which cities, etc.",
     link: 'https://cypressmill.co/project-showcase/senators-coaches/',
     technologies: ['Python', 'React', 'JS'],
   },
   {
     id: '5',
     title: 'Workorder System',
-    description: 'A modernized work order system that automates \
-    processes with QuickBooks. Using the following technologies, my team and I saw the completion of \
-    this project from start to finish.',
+    description: "Created a client's workorder system from the ground up alongside my team lead and our in house designer. \
+    The system needed to seamlessly transision the client away from their existing system while maintaining Quickbooks integration",
     technologies: ['React', 'TS', 'Ionic', 'Django', 'Python'],
   },
   {
     id: '6',
     title: 'Horse Managing Application',
-    description: 'An application for managing horses. This app keeps track of the horse, maintenance records, stables, and more.',
+    description: 'Implemented many features such as event scheduling, user logins, API endpoints, file uploads, and more.',
     technologies: ['React', 'TS', 'Ionic', 'Django', 'Python'],
   }
 ]
