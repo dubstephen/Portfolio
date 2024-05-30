8ac258b23097e64369b5adb4a1fa6e2294f9c69d
Stephen Bloodworth <stephenbloodworth72@gmail.com>
2024-05-30 10:24:33 -0500

    Fix spacing across about me section

--- sb-portfolio/app/components/AboutMe.tsx
+++ sb-portfolio/app/components/AboutMe.tsx
@@ -29,15 +29,15 @@ const AboutMeSection = (props: Props) => {
   return (
     <>
       <section className='text-white bg-big-stone justify-center md:text-left flex about'>
-        <span id='about' className=" md:mb-0 mt-12 " />
-        <div  className={`transition-all duration-75 ${navBarIsOpen ? 'blur-sm ' : ''} ml-8 mr-8 md:mb-0 mt-36 md:mt-32`}>
+        <span id='about' className="md:mb-0 mt-20 md:mt-12" />
+        <div className={`transition-all duration-75 ${navBarIsOpen ? 'blur-sm ' : ''} ml-8 mr-8 md:mb-0 mt-36 md:mt-32`}>
           <h3>
             <div className={`flex items-center w-full transition-opacity ease-in duration-700 ${showSection ? "opacity-100" : "opacity-0"}`}>
-              <span className="text-bismark text-md sm:text-xl md:text-3xl pr-2 font-mono">01.</span>
+              <span className="text-bismark text-2xl sm:text-xl md:text-3xl pr-2 font-mono">01.</span>
               <div className={`transition-opacity  ease-in duration-700 ${showSection ? "opacity-100" : "opacity-0"}`}>
-                <span className="text-cadet-blue text-md sm:text-xl md:text-3xl font-sans">About Me</span>
+                <span className="text-cadet-blue text-2xl sm:text-xl md:text-3xl font-sans">About Me</span>
               </div>
-              <hr className="w-[60%] md:w-[13%] h-0.5 ml-2 my-4 bg-blue-Dianne border-0 rounded md:my-10 " />
+              <hr className="w-[43%] md:w-[31%] h-0.5 ml-2 my-4 bg-blue-Dianne border-0 rounded md:my-10" />
             </div>
           </h3>
           <AboutMeDescription />
