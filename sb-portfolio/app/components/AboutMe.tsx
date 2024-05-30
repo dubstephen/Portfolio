115f681766766c0549391ad140fe55b85dbc51c8
Stephen Bloodworth <stephenbloodworth72@gmail.com>
2024-05-30 10:23:14 -0500

    Re-order technologies in about me section

--- sb-portfolio/app/components/AboutMe.tsx
+++ sb-portfolio/app/components/AboutMe.tsx
@@ -1,74 +1,11 @@
 'use client';
 import React, { useEffect, useState } from 'react'
-import Image from "next/image";
 import './AboutMe.css'
-import TechRow from './TechRow';
 import AboutMeDescription from './AboutMeDescription';
 
 interface Props {
   navBarIsOpen: boolean;
 }
-
-const technologiesRow1: Technology[] = [
-  {
-    title: 'Next.js',
-    imageSRC: '/next-js-seeklogo.svg',
-    link: 'https://nextjs.org/',
-    id: '1'
-  },
-  {
-    title: 'TypeScript',
-    imageSRC: '/typescript.svg',
-    link: 'https://www.typescriptlang.org/',
-    id: '2'
-  },
-  {
-    title: 'Tailwind',
-    imageSRC: '/Tailwind_CSS_Logo.svg',
-    link: 'https://tailwindcss.com/',
-    id: '3'
-  },
-]
-const technologiesRow2: Technology[] = [
-  {
-    title: 'Django',
-    imageSRC: '/djangoproject.svg',
-    link: 'https://www.djangoproject.com/',
-    id: '4'
-  },
-  {
-    title: 'Python',
-    imageSRC: '/python-logo-only.svg',
-    link: 'https://www.python.org/',
-    id: '5'
-  },
-  {
-    title: 'Lua',
-    imageSRC: '/lua-icon.svg',
-    link: 'https://www.lua.org/',
-    id: '6'
-  },
-]
-const technologiesRow3: Technology[] = [
-  {
-    title: '.NET',
-    imageSRC: '/Microsoft_.NET_logo.svg',
-    link: 'https://dotnet.microsoft.com/en-us/',
-    id: '7'
-  },
-  {
-    title: 'C#',
-    imageSRC: '/Logo_C_sharp.svg',
-    link: 'https://dotnet.microsoft.com/en-us/languages/csharp',
-    id: '8'
-  },
-  {
-    title: 'Ionic',
-    imageSRC: '/ionicframework-icon.svg',
-    link: 'https://ionicframework.com/',
-    id: '9'
-  }
-]
 
 const AboutMeSection = (props: Props) => {
   const { navBarIsOpen } = props;
